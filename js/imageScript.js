128d195448460e2568e12e53be50d5b2e4470940
Richard <rmpanga@gmail.com>
2015-07-08 23:32:01 -0400

    Current Website

---
+++ js/imageScript.js
@@ -0,0 +1,21 @@
+function youTubeImageHover(){
+
+$("#YouTube-image").attr('src' , 'images/youPlay.png')
+
+}
+
+function youTubeImageOut(){
+
+$("#YouTube-image").attr('src' , 'images/youT.png')
+
+}
+
+function logoIn(){
+$("#logo").attr('src' , 'images/R_noise.jpg')
+
+}
+
+function logoOut(){
+$("#logo").attr('src' , 'images/R_medum.jpg')
+
+}
